ddde218d0f3a0e65c81a3876b646cdefee49ca06
fromgate <fromgate@gmail.com>
2016-06-29 14:13:49 +0300

    0.15.x

--- src/main/java/ru/nukkit/anyversion/AnyVersion.java
+++ src/main/java/ru/nukkit/anyversion/AnyVersion.java
@@ -10,26 +10,41 @@ import cn.nukkit.network.protocol.ProtocolInfo;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
 
+import java.lang.reflect.Field;
+
 public class AnyVersion extends PluginBase implements Listener{
+    private int serverProtocolVersion;
 
     @EventHandler (ignoreCancelled = true, priority = EventPriority.LOW)
     public void onLoginPacket(DataPacketReceiveEvent event){
         DataPacket packet = event.getPacket();
         if (packet.pid() != ProtocolInfo.LOGIN_PACKET) return;
         LoginPacket loginPacket = (LoginPacket) packet;
-        if (loginPacket.protocol1 == ProtocolInfo.CURRENT_PROTOCOL){
+        if (loginPacket.getProtocol() == this.serverProtocolVersion){
             this.getLogger().info(loginPacket.username+ " is using correct protocol version");
         } else {
-            this.getLogger().info(TextFormat.YELLOW+loginPacket.username+ " is using protocol version "+loginPacket.protocol1+" (server version: "+ProtocolInfo.CURRENT_PROTOCOL+")");
+            this.getLogger().info(TextFormat.YELLOW+loginPacket.username+ " is using protocol version "+loginPacket.getProtocol()+" (server version: "+this.serverProtocolVersion+")");
             this.getLogger().info(TextFormat.RED+"Warning! "+TextFormat.YELLOW+"Using outdated/outrunning client could damage your server.");
             this.getLogger().info(TextFormat.YELLOW+"Use it on your own risk");
-            loginPacket.protocol1 = ProtocolInfo.CURRENT_PROTOCOL;
+            loginPacket.protocol = this.serverProtocolVersion;
         }
     }
 
     @Override
     public void onEnable(){
+        this.serverProtocolVersion =getVersion();
+        this.getLogger().info("Server protocol version: "+this.serverProtocolVersion);
         this.getServer().getPluginManager().registerEvents(this,this);
     }
 
+
+    private int getVersion(){
+        try {
+            Field f = ProtocolInfo.class.getDeclaredField("CURRENT_PROTOCOL");
+            return f.getInt(null);
+        } catch (Exception e) {
+            this.getLogger().info("Failed to detect actual value of server protocol version. Will use precompiled value.");
+            return ProtocolInfo.CURRENT_PROTOCOL;
+        }
+    }
 }
